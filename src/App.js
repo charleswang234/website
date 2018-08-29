e9015daa7315d48d19dfdfd38ef0d27a484a74be
charleswang234 <wang.charleswang234@gmail.com>
2018-08-29 16:57:40 -0400

    added react router and made simple routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'React';
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
+import Home from "./Home.js";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <BrowserRouter>
+        <div className="App">
+          {/* <h1>Hi, I'm Charles</h1> */}
+          {/*
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -13,7 +18,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      */}
+          <Switch>
+            <Route exact path="/" component= { Home } />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
