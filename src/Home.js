fa7c43cf41febbb5fa5fdc20a415a3afce05aa51
charleswang234 <wang.charleswang234@gmail.com>
2018-09-01 00:23:39 -0400

    rough css of title page section

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import TitlePage from "./TitlePage.js";
+import Skills from "./Skills.js";
 
 class Home extends Component {
     render() {
         return (
             <div className="home">
                 <TitlePage />
+                <Skills />
             </div>
         )
     }
--- src/TitlePage.js
+++ src/TitlePage.js
@@ -4,31 +4,30 @@ class TitlePage extends Component {
 	render() {
 		return (
 			<div className="title-page-container">
-				{/* <div className="color-overlay"> */}
-				<h1>Hi, I'm Charles.</h1>
-				<h2>SOFWARE DEVELOPER</h2>
-				<p>_____________________________</p>
-				<p>I'm a Computer Science student at University of Waterloo. I </p>
-				<div className="contact-container">
-					<p>
-						<i className="fas fa-envelope"></i>
-						wang.charles234@gmail.com
-          </p>
-					<p>
-						<i class="fas fa-phone"></i>
-						+1 647-633-3568
-					</p>
-					<p>
-						<i class="fab fa-github"></i> 
-						charleswang234
-					</p>
-					<p>
-						<i class="fab fa-linkedin"></i>
-						charlesccwang
-					</p>
+				<div className="color-overlay"></div>
+				<div className="introduction"> 
+					<h1>Hi, I'm Charles.</h1>
+					<p className="job">SOFTWARE DEVELOPER</p>
+					<hr className="line-break"></hr>
+					<div className="contact-container">
+						<p className="contact-item">
+							<i className="fas fa-envelope"></i>
+							wang.charles234@gmail.com
+          					</p>
+						<p className="contact-item">
+							<i class="fas fa-phone"></i>
+							+1 647-633-3568
+							</p>
+						<p className="contact-item">
+							<i class="fab fa-github"></i>
+							charleswang234
+							</p>
+						<p className="contact-item">
+							<i class="fab fa-linkedin"></i>
+							charlesccwang
+							</p>
+					</div>
 				</div>
-				{/* </div> */}
-
 			</div>
 		)
 	}
